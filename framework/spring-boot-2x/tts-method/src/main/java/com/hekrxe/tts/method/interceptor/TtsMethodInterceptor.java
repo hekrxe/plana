a4cb9ada583ac14ae2df25b916d8a7182ad447b9
hekrxe <hekrxe@163.com>
2019-02-15 16:06:52 +0800

    target

--- framework/spring-boot-2x/tts-method/src/main/java/com/hekrxe/tts/method/interceptor/TtsMethodInterceptor.java
+++ framework/spring-boot-2x/tts-method/src/main/java/com/hekrxe/tts/method/interceptor/TtsMethodInterceptor.java
@@ -2,6 +2,7 @@ package com.hekrxe.tts.method.interceptor;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.framework.AopProxyUtils;
 
 import java.lang.reflect.Method;
 
@@ -15,11 +16,21 @@ public class TtsMethodInterceptor implements MethodInterceptor {
         Method method = invocation.getMethod();
         Object target = invocation.getThis();
 
+        Class<?> targetClass = getTargetClass(target);
+
         long t1 = System.currentTimeMillis();
         Object result = invocation.proceed();
         long t2 = System.currentTimeMillis();
-        System.out.println(target.getClass().getSimpleName() + "." + method.getName() + ": " + (t2 - t1));
+        System.out.println(targetClass.getName() + "." + method.getName() + ": " + (t2 - t1));
 
         return result;
+    }
+
+    private Class<?> getTargetClass(Object target) {
+        Class<?> targetClass = AopProxyUtils.ultimateTargetClass(target);
+        if (targetClass == null) {
+            targetClass = target.getClass();
+        }
+        return targetClass;
     }
 }
